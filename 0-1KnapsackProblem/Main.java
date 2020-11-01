4b9af77121898a85ccd3af3072ecd42a4608f042
sugimori <taro@fc.jpn.org>
2020-11-01 03:27:09 +0000

    ナップザック（二重ループ）

--- 0-1KnapsackProblem/Main.java
+++ 0-1KnapsackProblem/Main.java
@@ -12,7 +12,7 @@ public class Main {
         for(int i=0;i<1001;i++) {
             dp[i] = new int[10001];
             for(int j=0;j<10001;j++) {
-                dp[i][j] = -1;
+                dp[i][j] = 0;
             }
         }
         Scanner sc = new Scanner(System.in);
@@ -30,22 +30,20 @@ public class Main {
         System.out.println(maxvalue);
     }
 
-    static int rec(int i,int j) {
-        if(dp[i][j] != -1) return dp[i][j];
-        int res;
-        // System.out.println(i + "," + j + "=");
-        if(i == n) {
-            return 0;
-        } 
-        if(w[i] > j) {
-            res = rec(i+1,j);
-        } else {
-            res = Math.max(
-                rec(i+1,j),
-                rec(i+1,j-w[i]) + v[i]);                
+    static int rec(int a,int b) {
+
+        for(int i = n-1;i>=0;i--){
+            for(int j = 0; j<=b; j++) {
+                if(j < w[i]) {
+                    dp[i][j] = dp[i+1][j];
+                } else {
+                    dp[i][j] = Math.max(
+                        dp[i+1][j], 
+                        dp[i+1][j-w[i]]+v[i]);
+                }
+            }
         }
-        // System.out.println(i + "," + j + "=" + res);
-        dp[i][j] = res;
-        return res;
+
+        return dp[a][b];
     }
 }
