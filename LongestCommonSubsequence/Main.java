cc1d283e2dd8f4ddc37ad421727f8ae7d35b47ca
sugimori <taro@fc.jpn.org>
2020-11-22 02:46:57 +0000

    ループを減らした

--- LongestCommonSubsequence/Main.java
+++ LongestCommonSubsequence/Main.java
@@ -2,14 +2,6 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-// 	    0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15
-// 1	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15
-// 2	0	1	1	2	2	3	3	4	4	5	5	6	6	7	7	8
-// 7	0	1	1	2	2	3	3	1	2	2	3	3	4	4	2	3
-// 8	0	1	1	2	2	3	3	1	1	2	2	3	3	4	2	2
-// 12	0	1	1	2	2	3	3	1	1	2	2	3	1	2	2	2
-// 50																
-
 public class Main {
     static final int MAXQ = 150;
     static final int MAXLEN = 1000;
@@ -42,17 +34,14 @@ public class Main {
             for(int j = 1; j<= x.length(); j++) {
                 if(x.charAt(j-1) == y.charAt(i-1)) {
                     int count = 0;
-                    for(int k=1;k<j;k++) {
-                        count = Math.max(count,dp[i-1][k]);
-                    }
-                    dp[i][j] = Math.max(count+1,dp[i-1][j]);
+                    dp[i][j] = Math.max(dp[i-1][j-1]+1,dp[i-1][j]);
                 } else {
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
                 }
                 max = Math.max(max,dp[i][j]);
             }
         }
-        debugprint();
+        // debugprint();
 
         return max;
     }
