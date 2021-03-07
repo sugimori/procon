867e695c9ad11606863da05339c8f15184540aef
杉森隆行 <sugimori@fc.jpn.org>
2021-03-07 15:19:02 +0900

    リベンジ

--- RevengeofTravelingSalesmanProblem/Main.java
+++ RevengeofTravelingSalesmanProblem/Main.java
@@ -5,28 +5,27 @@ import java.util.Arrays;
 public class Main {
     static final int MAXN = 16;
     static final int MAXM = (int)Math.pow(16,2);
-    static final int INF = Integer.MAX_VALUE/2;
+    static final long INF = Long.MAX_VALUE/2;
     static int n,m; 
-    static int distance[][] = new int[MAXN+1][MAXN+1]; 
-    static int timeout[][] = new int[MAXN+1][MAXN+1]; 
-    static int dp[][][] = new int[1 << MAXN][MAXN+1][2];  
-    static boolean debug = true;
-    static int count = 0;
-
-    // v=2 都市2個 11(3) 10(2) 01(1) 00(0)
+    static long distance[][] = new long[MAXN+1][MAXN+1]; 
+    static long timeout[][] = new long[MAXN+1][MAXN+1]; 
+    static long dpcount[][] = new long[1 << MAXN][MAXN+1];  
+    static long dpcost[][] = new long[1 << MAXN][MAXN+1];  
+    static boolean debug = false;
 
     public static void main(String[] args) {
         //初期化
-        for(int[][] tmpdp : dp) {
-            for(int[] pair : tmpdp) {
-                pair[0] = INF;
-                pair[1] = 0;
-            }
+        for(long[] tmpdp : dpcount) {
+            Arrays.fill(tmpdp,0);            
         }
-        for(int[] tmpd : distance) {
+        for(long[] tmpdp : dpcost) {
+            Arrays.fill(tmpdp,INF);            
+        }
+        
+        for(long[] tmpd : distance) {
             Arrays.fill(tmpd,-1);
         }
-        for(int[] tmpt : timeout) {
+        for(long[] tmpt : timeout) {
             Arrays.fill(tmpt,-1);
         }
 
@@ -37,71 +36,70 @@ public class Main {
         for(int i=1;i<=m;i++) {
             int tmps = sc.nextInt();
             int tmpt = sc.nextInt();
-            int tmpd = sc.nextInt();
-            int tmptime = sc.nextInt();
-            distance[Math.min(tmps,tmpt)][Math.max(tmps,tmpt)] = tmpd;
-            timeout[Math.min(tmps,tmpt)][Math.max(tmps,tmpt)] = tmptime;
+            long tmpd = sc.nextLong();
+            long tmptime = sc.nextLong();
+            distance[tmps][tmpt] = tmpd;
+            distance[tmpt][tmps] = tmpd;
+            timeout[tmps][tmpt] = tmptime;
+            timeout[tmpt][tmps] = tmptime;
         }
 
-        for(int i=2;i<=n;i++) {
-            dp[1<<(i-1)][i][0] = distance[1][i];
+        //初期値
+        dpcost[0][1] = 0; // まだどこも行ってない
+        dpcount[0][1] = 1;
+        long[] result = solve((1<<n)-1,1);
+        if(result[0] == INF ) {
+            System.out.println("IMPOSSIBLE");
+        } else {
+            System.out.println(result[0] + " " + result[1]);
         }
-        int[] result = solve((1<<n)-1,1);
-        if(result[0] == INF ) result[0] = -1;
-        System.out.println(result);
     
     }
 
-    static int[] solve(int sub,int end) {
-        int count = 0;
+    static long[] solve(int sub,int end) {
         if(debug) System.out.printf("IN(%s,%d)\n",d2bin(sub),end);
         debugprint();
-        if(dp[sub][end][0] != INF) {
-            return dp[sub][end];
+        if(dpcost[sub][end] != INF) {
+            return new long[]{dpcost[sub][end],dpcount[sub][end]};
         }
         if(debug) System.out.printf("sub:%s -> end:%s\n",d2bin(sub),d2bin(sub&(1<<(end-1))));
         if(contain(sub,end)) {
-            if((sub & ~(1 << (end-1)) )!= 0) {
-                count = 0;
-                for(int u=1;u<=n;u++) {
-                    if(contain(sub,u)) {
-                        if(distance[Math.min(u,end)][Math.max(u,end)] != -1) {
-                            int[] result = solve(sub & ~(1 << (end-1)),u);
-                            if(result[0] != -1) {
-                                if(dp[sub][end][0] > result[0]+distance[Math.min(u,end)][Math.max(u,end)]) {
-                                    dp[sub][end][0] = result[0]+distance[Math.min(u,end)][Math.max(u,end)];
-                                    dp[sub][end][1] = result[1];
-                                } else if(dp[sub][end][0] == result[0]+distance[Math.min(u,end)][Math.max(u,end)]) {
-                                    dp[sub][end][1] += result[1];
-                                }                                
-                            }
+            int nextsub = sub & ~(1<<(end-1));
+            for(int u=1;u<=n;u++) {     // 直前の到着地
+                if(contain(nextsub,u)) {
+                    if(distance[u][end] != -1) {    // 行ける
+                        long[] result = solve(nextsub,u);
+                        if((result[0] != -1)&&(result[0]+distance[u][end] <= timeout[u][end])) {
+                            if(dpcost[sub][end] > result[0]+distance[u][end]) {
+                                dpcost[sub][end] = result[0]+distance[u][end];
+                                dpcount[sub][end] = result[1];
+                            } else if(dpcost[sub][end] == result[0]+distance[u][end]) {
+                                dpcount[sub][end] += result[1];
+                            }                                
                         }
                     }
                 }
-            } else {
-                dp[sub][end][0] = distance[1][end];
-                dp[sub][end][1] = 1;
             }
         } else {
-            int[] res = {-1,0};
-            return res;
+            return new long[]{-1,0};
         }
-        if(debug) System.out.printf("OUT(%s,%d)=%d\n",d2bin(sub),end,dp[sub][end]);
+        if(debug) System.out.printf("OUT(%s,%d)=%d\n",d2bin(sub),end,dpcost[sub][end]);
         debugprint();
-        return dp[sub][end];
+        return new long[]{dpcost[sub][end],dpcount[sub][end]};
     }
     static boolean contain(int s, int v) {
-        return (s & ~(1 << (v-1)) )!= 0;
+        if((s == 0) && (v == 1)) return true; // START
+        return (s & (1 << (v-1))) != 0;
     }
     static void debugprint() {
         if(debug) {
             for(int i=0;i<=(1<<n);i++) {
                 System.out.printf("%s:",d2bin(i));
                 for(int j=1;j<=n;j++) {
-                    if(dp[i][j][0] == INF) {
+                    if(dpcost[i][j] == INF) {
                         System.out.printf("---,");
                     } else {
-                        System.out.printf("%3d,",dp[i][j][0]);
+                        System.out.printf("%3d(%d),",dpcost[i][j],dpcount[i][j]);
                     }
                 }
                 System.out.println("");
